57b431cadce43d63a12cc03efd327d462882ee1f
Marco De Pasquale <depasquale.marco@gmail.com>
2020-06-30 16:56:33 +0200

    add histograms

--- 2_matplotlib/3_histogram.py
+++ 2_matplotlib/3_histogram.py
@@ -1,2 +1,25 @@
 # HISTOGRAM
 print('HISTOGRAM')
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.random.randn(10000)
+
+plt.xlabel('x')
+plt.ylabel('Counts', rotation=90)
+plt.hist(x, bins=50)
+plt.grid(color='lightgray', linestyle='--', linewidth=0.5)
+plt.savefig('histogram_normal.png',dpi=250)
+plt.clf()
+plt.close()
+
+xu = np.random.random(10000)
+
+plt.xlabel('x')
+plt.ylabel('Counts', rotation=90)
+plt.hist(xu, bins=50)
+plt.grid(color='lightgray', linestyle='--', linewidth=0.5)
+plt.savefig('histogram_uniform.png',dpi=250)
+plt.clf()
+plt.close()
