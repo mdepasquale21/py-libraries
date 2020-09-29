a4d7f3844742280c5ea373d0b88626eec7af3e7a
Marco De Pasquale <depasquale.marco@gmail.com>
2020-09-29 10:59:10 +0200

    add scatterplot with decision boundary

--- 2_matplotlib/2_scatterplot.py
+++ 2_matplotlib/2_scatterplot.py
@@ -39,6 +39,30 @@ plt.savefig('scatterplot-2D-classes.png', dpi = 250)
 plt.clf()
 plt.close()
 
+print('2-D scatterplot with Decision Boundary')
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(data2D_classes, labels)
+from matplotlib.colors import ListedColormap
+
+plt.figure(figsize=(10,10))
+X1, X2 = np.meshgrid(np.arange(start = data2D_classes[:, 0].min() - 1, stop = data2D_classes[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = data2D_classes[:, 1].min() - 1, stop = data2D_classes[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(labels)):
+    plt.scatter(data2D_classes[labels == j, 0], data2D_classes[labels == j, 1],
+                c = [ListedColormap(('darkred', 'darkgreen'))(i)], label = int(j))
+plt.title('Decision Boundary')
+plt.xlabel('X1')
+plt.ylabel('X2', rotation=0)
+plt.legend()
+plt.savefig('scatterplot-2D-decision-boundary.png', dpi=250)
+plt.clf()
+plt.close()
+
 print('3-D')
 data3D = np.random.randn(300,3)
 
